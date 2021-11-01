6d01f1dbbbbe361442973e98eba8ac57ef104e37
diegargon <diego@envigo.net>
2021-11-01 13:23:48 +0100

    init

---
+++ config/config.priv.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *
+ *  @author diego/@/envigo.net
+ *  @package
+ *  @subpackage
+ *  @copyright Copyright @ 2020 - 2021 Diego Garcia (diego/@/envigo.net)
+ */
+!defined('IN_WEB') ? exit : true;
+
+/* Systems */
+
+$cfg['os'] = [
+    1 => 'windows',
+    2 => 'linux',
+];
+$cfg['os_distributions'] = [
+    1 => 'microsoft',
+    2 => 'ubuntu',
+    3 => 'debian',
+];
+
+$cfg['access_methods'] = [
+    1 => 'ssh',
+];
---
+++ config/example.inc.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ *
+ *  @author diego/@/envigo.net
+ *  @package
+ *  @subpackage
+ *  @copyright Copyright @ 2020 - 2021 Diego Garcia (diego/@/envigo.net)
+ */
+!defined('IN_WEB') ? exit : true;
+
+/* Db Config */
+
+$cfg_db['dbtype'] = 'mysql';
+$cfg_db['dbhost'] = 'localhost';
+$cfg_db['dbname'] = 'monnet';
+$cfg_db['dbuser'] = '';
+$cfg_db['dbpassword'] = '';
+$cfg_db['dbprefix'] = '';
+$cfg_db['dbcharset'] = 'utf8';
+
+/* Config */
+
+$cfg['web_title'] = 'MonNet';
+$cfg['path'] = '';
+$cfg['rel_path'] = '';
+$cfg['lang'] = 'es';
+$cfg['sid_expire'] = 0;
+$cfg['theme'] = 'default';
+$cfg['css'] = 'default';
+$cfg['charset'] = 'utf-8';
+
+/* Modules Config */
+/* TODO: Split load config files */
+$cfg['weather_widget']['country'] = '';
+$cfg['weather_widget']['weather_api'] = '';
