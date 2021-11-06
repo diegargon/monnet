0ccacc97214e609bc36e446c72c4d838a41a07d2
diegargon <diego@envigo.net>
2021-11-06 17:52:32 +0100

    init

--- tpl/default/host-details.tpl.php
+++ tpl/default/host-details.tpl.php
@@ -9,22 +9,22 @@
 ?>
 <div id="host-details" class="host-details">
     <div id="hosts-details-container" class="host-details-container">
-        <a onclick="" href="javascript:void(0);" rel="noreferrer" class="hosts-item" title="<?= $tdata['host_details']['details'] ?>">
-            <div class="host-details-main">
-                <img class="hosts-online" src="<?= $tdata['host_details']['online_image'] ?>" alt="<?= $tdata['host_details']['alt_online'] ?>"/>
-                <?php if (!empty($tdata['host_details']['os_image'])) { ?>
-                    <img class="fab" src="<?= $tdata['host_details']['os_image'] ?>" alt="<?= $tdata['host_details']['os_name'] ?>"/>
-                <?php } ?>
-                <?php if (!empty($tdata['host_details']['img_ico'])) { ?>
-                    <img class="fab" src="<?= $tdata['host_details']['img_ico'] ?>" alt=""/>
-                <?php } ?>
-                <div class="host-title"><?= $tdata['host_details']['title'] ?> </div>
-                <?php if (!empty($tdata['host_details']['hostname'])) { ?>
-                    <div class="host-hostname"><?= $tdata['host_details']['hostname'] ?> </div>
-                <?php } ?>
-                <div class="host-ip"><?= $tdata['host_details']['ip'] ?></div>
-            </div>
-        </a>
+
+        <div class="host-details-main">
+            <img class="hosts-online" src="<?= $tdata['host_details']['online_image'] ?>" alt="<?= $tdata['host_details']['alt_online'] ?>"/>
+            <?php if (!empty($tdata['host_details']['os_image'])) { ?>
+                <img class="fab" src="<?= $tdata['host_details']['os_image'] ?>" alt="<?= $tdata['host_details']['os_name'] ?>"/>
+            <?php } ?>
+            <?php if (!empty($tdata['host_details']['img_ico'])) { ?>
+                <img class="fab" src="<?= $tdata['host_details']['img_ico'] ?>" alt=""/>
+            <?php } ?>
+            <div class="host-title"><?= $tdata['host_details']['title'] ?> </div>
+            <?php if (!empty($tdata['host_details']['hostname'])) { ?>
+                <div class="host-hostname"><?= $tdata['host_details']['hostname'] ?> </div>
+            <?php } ?>
+            <div class="host-ip"><?= $tdata['host_details']['ip'] ?></div>
+        </div>
+
         <?php
         if (!empty($tdata['host_details']['host_ports'])) {
             ?>
