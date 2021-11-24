ec77e875635adbbfa8ff9114466f136710b82a6a
diegargon <diego@envigo.net>
2021-11-24 19:56:01 +0100

    init

--- config/example.inc.php
+++ config/example.inc.php
@@ -9,28 +9,32 @@
  */
 !defined('IN_WEB') ? exit : true;
 
+require_once('config/config.priv.php');
 /* Db Config */
 
 $cfg_db['dbtype'] = 'mysql';
 $cfg_db['dbhost'] = 'localhost';
 $cfg_db['dbname'] = 'monnet';
-$cfg_db['dbuser'] = '';
+$cfg_db['dbuser'] = 'monnet';
 $cfg_db['dbpassword'] = '';
 $cfg_db['dbprefix'] = '';
 $cfg_db['dbcharset'] = 'utf8';
 
 /* Config */
 
-$cfg['web_title'] = 'MonNet';
-$cfg['path'] = '';
-$cfg['rel_path'] = '';
+$cfg['web_title'] = 'Monnet';
+$cfg['path'] = '/var/www/html';
+$cfg['rel_path'] = '/';
 $cfg['lang'] = 'es';
-$cfg['sid_expire'] = 0;
+$cfg['sid_expire'] = 99999;
 $cfg['theme'] = 'default';
 $cfg['css'] = 'default';
 $cfg['charset'] = 'utf-8';
 $cfg['net'] = '192.168.1.0/24';
+
+$cfg['cert'] = '/home/monnet/.ssh/id_rsa';
+
 /* Modules Config */
 /* TODO: Split load config files */
-$cfg['weather_widget']['country'] = '';
-$cfg['weather_widget']['weather_api'] = '';
+$cfg['weather_widget']['country'] = 'vigo';
+$cfg['weather_widget']['weather_api'] = '89fe8d3a8486486fc682ba97dc28850f';
