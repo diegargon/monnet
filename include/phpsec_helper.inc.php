9fced0a82da265e46243133a3ec8021b898d491f
diegargon <diego@envigo.net>
2024-01-14 14:01:18 +0100

    copyright

--- include/phpsec_helper.inc.php
+++ include/phpsec_helper.inc.php
@@ -5,6 +5,6 @@
  *  @author diego/@/envigo.net
  *  @package
  *  @subpackage
- *  @copyright Copyright @ 2020 - 2021 Diego Garcia (diego/@/envigo.net)
+ *  @copyright Copyright @ 2020 - 2023 Diego Garcia (diego/@/envigo.net)
  */
 !defined('IN_CLI') ? exit : true;
