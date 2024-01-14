82e53dd561e4becb1dd5c06dd2ad7bac6897f61f
diegargon <diego@envigo.net>
2024-01-14 14:01:43 +0100

    copyright

--- modules/weather_widget/weather_widget.js
+++ modules/weather_widget/weather_widget.js
@@ -3,7 +3,7 @@
  *  @author diego/@/envigo.net
  *  @package 
  *  @subpackage 
- *  @copyright Copyright @ 2020 - 2023 Diego Garcia (diego/@/envigo.net)
+ *  @copyright Copyright @ 2020 - 2024 Diego Garcia (diego/@/envigo.net)
  */
 
 
