6ead548edba67efe813e59af82a865f1b1d5c4b2
diegargon <diego@envigo.net>
2024-01-23 22:47:23 +0100

    chagne refresh time

--- tpl/default/refresher.tpl.php
+++ tpl/default/refresher.tpl.php
@@ -64,7 +64,7 @@
 
         // avoid launch timer when command FIX:better way for not launch timers, disable timer and allow launch
         if (command === false) {
-            setTimeout(refresh, 150000);
+            setTimeout(refresh, 100000);
         }
 
     }
