d89faf0cb85b8be844e57c2961fd75a41b1e1865
diegargon <diego@envigo.net>
2024-01-28 13:38:32 +0100

    curl options

--- include/curl.inc.php
+++ include/curl.inc.php
@@ -10,8 +10,14 @@
 !defined('IN_CLI') ? exit : true;
 
 function curl_get(string $url) {
+    $agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; Monnet)';
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_USERAGENT, $agent);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 2); // Try connect (s)
+    curl_setopt($ch, CURLOPT_TIMEOUT, 10); // Return (s)
+
     return curl_exec($ch);
 }
