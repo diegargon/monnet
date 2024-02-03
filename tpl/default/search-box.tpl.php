e2976fe58387471489dbdf4706ebd31076496dc4
diegargon <diego@envigo.net>
2024-02-03 12:13:02 +0100

    minor

--- tpl/default/search-box.tpl.php
+++ tpl/default/search-box.tpl.php
@@ -12,7 +12,7 @@
 if (valid_array($tdata['search_engines'])) {
     ?>
     <div class="search_container">
-        <h1 class='title gradiant'><a href=""><?= $tdata['head_name'] ?></a></h1>
+        <h1 class="title gradiant"><a href=""><?= $tdata['head_name'] ?></a></h1>
         <div class="search-wrapper">
             <form target="_blank"  action="<?= $tdata['search_engines'][0]['url'] ?>" method="GET">
                 <input type="text" name="<?= $tdata['search_engines'][0]['name'] ?>" required class="search-box" placeholder="Google" autofocus/>
