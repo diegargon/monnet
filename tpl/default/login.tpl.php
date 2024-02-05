f1d6f273eb53bbff45c8eb8d4d48c6bfff3c104f
diego.garcia <diego.garcia@A02P0102.ALTIA.VIGO>
2024-02-05 12:12:26 +0100

    add check

--- tpl/default/login.tpl.php
+++ tpl/default/login.tpl.php
@@ -31,7 +31,7 @@
     <div class="profile_box">
         <form  method="POST" action="?page=login">
             <div class="profile_name">
-                <input size="18"  onfocus="this.value = ''" placeholder="<?= $tdata['username_placeholder'] ?>" class="login_username" type="text" name="username" value="<?= $tdata['username'] ?>" <?= $tdata['set_username_focus'] ? 'autofocus' : null ?>/>
+                <input size="18"  onfocus="this.value = ''" placeholder="<?= $tdata['username_placeholder'] ?>" class="login_username" type="text" name="username" value="<?= $tdata['username'] ?>" <?= isset($tdata['set_username_focus']) ? 'autofocus' : null ?>/>
             </div>
             <div class="profile_password">
                 <input size="18"  onfocus="this.value = ''" placeholder="<?= $tdata['password_placeholder'] ?>" class="login_password" type="password" name="password" value="" <?= isset($tdata['set_pass_focus']) ? 'autofocus' : null ?>/>
