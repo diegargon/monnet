385ba0d09231781a7d766306d9cea6c6057e8a80
diegargon <diego@envigo.net>
2024-02-10 12:08:16 +0100

    add inc for split

---
+++ include/post-func.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ *
+ *  @author diego/@/envigo.net
+ *  @package
+ *  @subpackage
+ *  @copyright Copyright CC BY-NC-ND 4.0 @ 2020 - 2024 Diego Garcia (diego/@/envigo.net)
+ */
+!defined('IN_WEB') ? exit : true;
+
