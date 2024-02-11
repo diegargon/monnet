bd1bb0e97dc7d0c93b62e261c199c9bab768c655
diegargon <diego@envigo.net>
2024-02-11 14:14:38 +0100

    split func

--- include/pages-post.inc.php
+++ include/pages-post.inc.php
@@ -11,71 +11,14 @@
 
 function page_index_post(Database $db, User $user, Categories $categories, array $lng) {
     $page_data = [];
+
     $profile_type = Filters::postString('profile_type');
     $show_bookmarks = Filters::postInt('show_bookmarks');
     $show_this_system = Filters::postInt('show_this_system');
     $show_highlight_hosts = Filters::postInt('show_highlight_hosts');
     $show_other_hosts = Filters::postInt('show_rest_hosts');
     //add Item
-    if (isset($_POST['addBookmarkForm'])) {
-        $bookmarkName = Filters::postString('bookmarkName');
-        $cat_id = Filters::postInt('cat_id');
-        $urlip = Filters::postUrl('urlip');
 
-        if (!$urlip) {
-            $urlip = Filters::postIP('urlip');
-        }
-
-        //TODO check valid image name.
-        $image_type = Filters::postString('image_type');
-        if ($image_type == 'image_resource') { //Remote Image
-            $field_img = Filters::postImgUrl('field_img');
-        } else { //favicon, local image
-            $field_img = Filters::postPathFile('field_img');
-        }
-
-        $weight = Filters::postInt('weight');
-    }
-    //
-    if (isset($_POST['addBookmarkForm'])) {
-        $page_data['show_add_bookmark'] = 1;
-
-        if (empty($bookmarkName)) {
-            $page_data['error_msg'] = "{$lng['L_FIELD']} {$lng['L_NAME']} {$lng['L_ERROR_EMPTY_INVALID']}";
-        } else if (empty($urlip)) {
-            $page_data['error_msg'] = "{$lng['L_FIELD']} {$lng['L_URLIP']} {$lng['L_ERROR_EMPTY_INVALID']}";
-        } else if (empty($cat_id)) {
-            $page_data['error_msg'] = "{$lng['L_FIELD']} {$lng['L_TYPE']} {$lng['L_ERROR_EMPTY_INVALID']}";
-        } else if (empty('weight')) {
-            $page_data['error_msg'] = "{$lng['L_FIELD']} {$lng['L_WEIGHT']} {$lng['L_ERROR_EMPTY_INVALID']}";
-        } else if (empty($image_type)) {
-            $page_data['error_msg'] = "{$lng['L_FIELD']} {$lng['L_IMAGE_TYPE']} {$lng['L_ERROR_EMPTY_INVALID']}";
-        }
-
-        if ($image_type != 'favicon' && empty($field_img)) {
-            $page_data['error_msg'] = "{$lng['L_LINK']} {$lng['L_ERROR_EMPTY_INVALID']}";
-        }
-
-        $page_data['bookmarkName'] = $bookmarkName;
-        $page_data['cat_id'] = $cat_id;
-        $page_data['urlip'] = $urlip;
-        $page_data['image_type'] = $image_type;
-        $page_data['field_img'] = $field_img;
-        $page_data['weight'] = $weight;
-        if (empty($page_data['error_msg'])) {
-            $conf = ['url' => $urlip, 'image_type' => $image_type, 'image_resource' => $field_img];
-            $set = ['cat_id' => $cat_id, 'type' => 'bookmarks', 'title' => $bookmarkName, 'conf' => json_encode($conf), 'weight' => $weight];
-            $db->insert('items', $set);
-            $page_data['status_msg'] = 'OK';
-        } else {
-            $page_data['bookmarkName'] = $bookmarkName;
-            $page_data['cat_id'] = $cat_id;
-            $page_data['urlip'] = $urlip;
-            $page_data['image_type'] = $image_type;
-            $page_data['field_img'] = $field_img;
-            $page_data['weight'] = $weight;
-        }
-    }
 
     if ($profile_type !== false) {
         $user->setPref('profile_type', $profile_type);
@@ -93,5 +36,75 @@ function page_index_post(Database $db, User $user, Categories $categories, array
         $user->setPref('show_other_hosts_status', $show_other_hosts);
     }
 
+    if (Filters::postInt('addBookmarkForm')) {
+        post_bookmark($db, $lng, $page_data);
+    }
+
+    if (Filters::postInt('addNetworkForm')) {
+        post_network($db, $page_data);
+    }
+
     return $page_data;
+}
+
+function post_bookmark(Database $db, array $lng, array &$page_data) {
+    $bookmarkName = Filters::postString('bookmarkName');
+    $cat_id = Filters::postInt('cat_id');
+    $urlip = Filters::postUrl('urlip');
+
+    if (!$urlip) {
+        $urlip = Filters::postIP('urlip');
+    }
+
+    //TODO check valid image name.
+    $image_type = Filters::postString('image_type');
+    if ($image_type == 'image_resource') { //Remote Image
+        $field_img = Filters::postImgUrl('field_img');
+    } else { //favicon, local image
+        $field_img = Filters::postPathFile('field_img');
+    }
+
+    $weight = Filters::postInt('weight');
+
+    $page_data['show_add_bookmark'] = 1;
+
+    if (empty($bookmarkName)) {
+        $page_data['error_msg'] = "{$lng['L_FIELD']} {$lng['L_NAME']} {$lng['L_ERROR_EMPTY_INVALID']}";
+    } else if (empty($urlip)) {
+        $page_data['error_msg'] = "{$lng['L_FIELD']} {$lng['L_URLIP']} {$lng['L_ERROR_EMPTY_INVALID']}";
+    } else if (empty($cat_id)) {
+        $page_data['error_msg'] = "{$lng['L_FIELD']} {$lng['L_TYPE']} {$lng['L_ERROR_EMPTY_INVALID']}";
+    } else if (empty('weight')) {
+        $page_data['error_msg'] = "{$lng['L_FIELD']} {$lng['L_WEIGHT']} {$lng['L_ERROR_EMPTY_INVALID']}";
+    } else if (empty($image_type)) {
+        $page_data['error_msg'] = "{$lng['L_FIELD']} {$lng['L_IMAGE_TYPE']} {$lng['L_ERROR_EMPTY_INVALID']}";
+    }
+
+    if ($image_type != 'favicon' && empty($field_img)) {
+        $page_data['error_msg'] = "{$lng['L_LINK']} {$lng['L_ERROR_EMPTY_INVALID']}";
+    }
+
+    $page_data['bookmarkName'] = $bookmarkName;
+    $page_data['cat_id'] = $cat_id;
+    $page_data['urlip'] = $urlip;
+    $page_data['image_type'] = $image_type;
+    $page_data['field_img'] = $field_img;
+    $page_data['weight'] = $weight;
+    if (empty($page_data['error_msg'])) {
+        $conf = ['url' => $urlip, 'image_type' => $image_type, 'image_resource' => $field_img];
+        $set = ['cat_id' => $cat_id, 'type' => 'bookmarks', 'title' => $bookmarkName, 'conf' => json_encode($conf), 'weight' => $weight];
+        $db->insert('items', $set);
+        $page_data['status_msg'] = 'OK';
+    } else {
+        $page_data['bookmarkName'] = $bookmarkName;
+        $page_data['cat_id'] = $cat_id;
+        $page_data['urlip'] = $urlip;
+        $page_data['image_type'] = $image_type;
+        $page_data['field_img'] = $field_img;
+        $page_data['weight'] = $weight;
+    }
+}
+
+function post_network(Database $db, array &$page_data) {
+
 }
