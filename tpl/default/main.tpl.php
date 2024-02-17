7d2af159c854976151c9c7cca56b77472f4d4445
diegargon <diego@envigo.net>
2024-02-17 20:13:20 +0100

    min

--- tpl/default/main.tpl.php
+++ tpl/default/main.tpl.php
@@ -14,9 +14,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="shortcut icon" href="favicon.ico" />
         <meta name="referrer" content="never">
-        <title><?= $tdata['web_title'] ?></title>                
-        <?= $tdata['main_head'] ?>         
-    <!-- <style>* { border: 1px solid red;}</style>  -->        
+        <title><?= $tdata['web_title'] ?></title>
+        <?= $tdata['main_head'] ?>
+    <!-- <style>* { border: 1px solid red;}</style>  -->
     </head>
 
     <body>
@@ -25,6 +25,5 @@
             <?= $tdata['main_body'] ?>
         </div>
         <footer><?= $tdata['main_footer'] ?></footer>
-
     </body>
 </html>
