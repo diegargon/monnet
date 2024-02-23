667df71068c8a81ae5cec2e805841b3ffa34fe8c
diegargon <diego@envigo.net>
2024-02-23 19:12:51 +0100

    lang class (not in use yet)

---
+++ class/Lang.class.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ *
+ *  @author diego/@/envigo.net
+ *  @package
+ *  @subpackage
+ *  @copyright Copyright CC BY-NC-ND 4.0 @ 2020 - 2024 Diego Garcia (diego/@/envigo.net)
+ */
+!defined('IN_WEB') ? exit : true;
+
+class Lng {
+
+    private static array $language = null;
+    private static string $defaultLang = 'es';
+    private static string $selLangCode = null;
+
+    public static function loadLanguage(string $langCode) {
+        /*
+         * We load es as default language to avoid missing keys
+         * The if != the selected config language
+         *
+         */
+        //Default lang
+        $langFile = 'lang/lang.' . self::$defaultLang . '.php';
+        if (file_exists($langFile)) {
+            self::$language = include $langFile;
+        } else {
+            throw new Exception("Lang file '$langFile' not found");
+        }
+        //Config lang
+        $sel_langfile = 'lang/lang.' . $langCode . '.php';
+        if ($langCode !== self::defaultLang && file_exists($sel_langfile)) {
+            self::$selLangCode = $langCode;
+            $sel_lang = include $sel_langfile;
+            self::$language = array_merge(self::$language, $sel_lang);
+        }
+
+        return true;
+    }
+
+    public static function loadUserLang(string $langCode) {
+        if ($langCode === self::$defaultLang || $langCode === self::$selLanCode) {
+            return false;
+        }
+        $userlangfile = 'lang/lang.' . $langCode . '.php';
+        if (file_exists($userlangfile)) {
+            $user_lang = include $userlangfile;
+            self::$language = array_merge(self::$language, $user_lang);
+        }
+        return true;
+    }
+
+    public static function get(string $key) {
+        if (self::$language && isset(self::$language[$key])) {
+            return self::$language[$key];
+        }
+        return null;
+    }
+}
