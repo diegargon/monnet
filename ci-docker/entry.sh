42813667d7cda242395204b32f9ae03c1a3cac61
diegargon <diego@envigo.net>
2024-11-01 13:30:09 +0100

    wf

--- ci-docker/entry.sh
+++ ci-docker/entry.sh
@@ -2,6 +2,5 @@
 
 ping -c 2 mysql-service
 mysql -h 172.18.0.1 -uroot -pmonnetadmin monnet < /var/www/html/config/monnet.sql
-mysql -h 172.18.0.2 -uroot -pmonnetadmin monnet < /var/www/html/config/monnet.sql
-mysql -h 127.0.0.1 -uroot -pmonnetadmin monnet < /var/www/html/config/monnet.sql
+
 exec ./vendor/bin/phpunit
--- tests/IndexTest.php
+++ tests/IndexTest.php
@@ -5,6 +5,27 @@ require_once __DIR__ . '/../index.php';
 use PHPUnit\Framework\TestCase;
 
 class IndexTest extends TestCase {
+
+    public function testHomePage() {
+        $url = 'http://127.0.0.1/index.php';
+        $response = $this->makeRequest($url);
+        $this->assertEquals(200, $response['http_code']);
+        $this->assertStringContainsString('Bienvenido', $response['body']);
+    }
+
+    private function makeRequest($url) {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $body = curl_exec($ch);
+        $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+
+        return [
+            'body' => $body,
+            'http_code' => $http_code
+        ];
+    }
+
 #    public function testGreet() {
 #        $this->assertEquals("Hello, World!", greet());
 #    }
