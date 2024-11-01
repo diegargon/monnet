cb241d5021ce66b5256749c4cb2390922227482f
diegargon <diego@envigo.net>
2024-11-01 13:55:23 +0100

    wf

--- ci-docker/entry.sh
+++ ci-docker/entry.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+apache2-foreground
 # TODO cant access to mysql-service by name only ip
 ping -c 2 mysql-service
 mysql -h 172.18.0.1 -uroot -pmonnetadmin monnet < /var/www/html/config/monnet.sql
