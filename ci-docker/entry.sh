8b21102aa9a1dc863073f4769f684f5e57af4d8f
diegargon <diego@envigo.net>
2024-11-01 12:09:50 +0100

    wf

--- ci-docker/entry.sh
+++ ci-docker/entry.sh
@@ -3,6 +3,7 @@
 /etc/init.d/mysql start
 
 ping -c 1 192.168.1.1
+ping -c 1 172.18.0.1
 ping -c 4 mysql-service
 mysql -h mysql-service -uroot -pmonnetadmin monnet < /var/www/html/config/monnet.sql
 mysql -h 127.0.0.1 -uroot -pmonnetadmin monnet < /var/www/html/config/monnet.sql
