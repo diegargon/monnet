a24bb4ea5fc0c9e578e32cbfa7eeacb7f224d82e
diegargon <diego@envigo.net>
2024-11-01 14:13:35 +0100

    wf

--- ci-docker/entry.sh
+++ ci-docker/entry.sh
@@ -2,10 +2,11 @@
 
 apache2-foreground &
 sleep 2
-# TODO cant access to mysql-service by name only ip
-ping -c 2 mysql-service
+# TODO No se puede acceder por nombre por que estan en redes diferentes ¿que pasa si cambia la ip?
+ping -c 1 mysql-service
 
 if ! mysql -h 172.18.0.1 -uroot -pmonnetadmin monnet < /var/www/html/config/monnet.sql; then
     echo "Error al ejecutar el script SQL. Asegúrate de que MySQL esté disponible."
+fi
 
 exec ./vendor/bin/phpunit
