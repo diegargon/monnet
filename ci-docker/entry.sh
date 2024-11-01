204e6038c9539044d229277539d3a8f5433c4964
diegargon <diego@envigo.net>
2024-11-01 12:16:41 +0100

    wf

--- ci-docker/entry.sh
+++ ci-docker/entry.sh
@@ -4,7 +4,8 @@
 
 ping -c 1 192.168.1.1
 ping -c 1 172.18.0.1
+ping -c 1 172.18.0.2
 ping -c 4 mysql-service
-mysql -h mysql-service -uroot -pmonnetadmin monnet < /var/www/html/config/monnet.sql
+mysql -h 172.18.0.2 -uroot -pmonnetadmin monnet < /var/www/html/config/monnet.sql
 mysql -h 127.0.0.1 -uroot -pmonnetadmin monnet < /var/www/html/config/monnet.sql
 exec ./vendor/bin/phpunit
