eafa89bbe8841b70c9e3d1520939ab2988846c3b
diegargon <diego@envigo.net>
2024-11-02 14:54:23 +0100

    phpunit

--- ci-docker/entry.sh
+++ ci-docker/entry.sh
@@ -7,6 +7,8 @@ ping -c 1 mysql-service
 
 if ! mysql -h 172.18.0.1 -uroot -pmonnetadmin monnet < /var/www/html/config/monnet.sql; then
     echo "Error al ejecutar el script SQL. Asegúrate de que MySQL esté disponible."
+else
+    echo "Base de datos subida con exito"
 fi
 
 exec ./vendor/bin/phpunit
