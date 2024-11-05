f1405906560db8c5b21bbb26c1b96de602ef4776
diegargon <diego@envigo.net>
2024-11-05 21:44:20 +0100

    debug

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
  * @var AppContext|null $ctx An instance of AppCtx or null if not defined
  */
 define('IN_WEB', true);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require('include/common.inc.php');
 require('include/usermode.inc.php');
