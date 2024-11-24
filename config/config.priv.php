fc59115eb0a753063575c63fa1eed0d9fea7b9de
diegargon <diego@envigo.net>
2024-11-24 19:14:39 +0100

    up

--- config/config.priv.php
+++ config/config.priv.php
@@ -10,7 +10,7 @@
 !defined('IN_WEB') ? exit : true;
 
 $cfg['monnet_version'] = 0.36;
-$cfg['monnet_revision'] = 27;
+$cfg['monnet_revision'] = 28;
 $cfg['monnet_homepage'] = "https://github.com/diegargon/monnet";
 $cfg['app_name'] = 'monnet';
 
