2e6a266a394eaf8168d518ccdd9798778006ff02
diegargon <diego@envigo.net>
2024-12-27 23:10:33 +0100

    test3 old

--- ci-docker/entry.sh
+++ ci-docker/entry.sh
@@ -4,6 +4,7 @@ apache2-foreground &
 sleep 2
 # TODO No se puede acceder por nombre por que estan en redes diferentes ¿que pasa si cambia la ip?
 ping -c 1 mysql-service
+ping -c 1 172.18.0.2
 
 if ! mysql -h 172.18.0.2 -uroot -pmonnetadmin monnet < /var/www/html/config/monnet.sql; then
     echo "Error al ejecutar el script SQL. Asegúrate de que MySQL esté disponible."
