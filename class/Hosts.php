31c5f5f3d017d556ea1abd3492361e78522b1ca2
diegargon <diego@envigo.net>
2025-01-08 21:50:56 +0100

    log msg info

--- class/Hosts.php
+++ class/Hosts.php
@@ -215,13 +215,13 @@ class Hosts
                     $this->hosts[$id]['alert'] = 1;
                     $alert_msg = '';
                     if ($kvalue === 'mac') :
-                        $alert_msg = 'Mac ' . $this->lng['L_HAS_CHANGED'];
+                        $alert_msg = 'Mac ' . $this->lng['L_HAS_CHANGED'] . "to $vvalue" ;
                     endif;
                     if ($kvalue === 'mac_vendor') :
-                        $alert_msg = 'Mac vendor ' . $this->lng['L_HAS_CHANGED'];
+                        $alert_msg = 'Mac vendor ' . $this->lng['L_HAS_CHANGED'] . "to $vvalue";
                     endif;
                     if ($kvalue === 'hostname') :
-                        $alert_msg = 'Hostname ' . $this->lng['L_HAS_CHANGED'];
+                        $alert_msg = 'Hostname ' . $this->lng['L_HAS_CHANGED'] . "to $vvalue";
                     endif;
                     Log::loghost('LOG_ALERT', $id, $alert_msg, 3);
                     if (!empty($this->hosts[$id]['alarm_macchange_email'])) :
