749c352fe379b2e6e3d10d97d95d8eed74f725c1
diegargon <diego@envigo.net>
2025-01-18 16:52:24 +0100

    up

--- config/config.priv.php
+++ config/config.priv.php
@@ -13,7 +13,7 @@ $cfg['monnet_version'] = 0.51;
 
 $cfg['monnet_revision'] = 76;
 $cfg['monnet_homepage'] = "https://github.com/diegargon/monnet";
-$cfg['agent_min_version'] = 0.125;
+$cfg['agent_min_version'] = 0.126;
 $cfg['agent_latest_version'] = 0.134;
 $cfg['app_name'] = 'monnet';
 
