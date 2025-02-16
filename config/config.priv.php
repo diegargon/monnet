23955229992b1fca4565fc1cb591906b9351b750
diegargon <diego@envigo.net>
2025-02-16 22:53:59 +0100

    cmd rewrite

--- config/config.priv.php
+++ config/config.priv.php
@@ -11,7 +11,7 @@
 
 $cfg['monnet_version'] = 0.53;
 
-$cfg['monnet_revision'] = 2;
+$cfg['monnet_revision'] = 5;
 $cfg['monnet_homepage'] = 'https://github.com/diegargon/monnet';
 $cfg['agent_min_version'] = 0.126;
 $cfg['agent_latest_version'] = 0.135;
