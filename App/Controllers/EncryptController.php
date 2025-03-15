9f0f133f5baf5c5b64c74345a958efb84a5a60e7
diegargon <diego@envigo.net>
2025-03-15 14:57:14 +0100

    initial

--- App/Controllers/EncryptController.php
+++ App/Controllers/EncryptController.php
@@ -5,11 +5,15 @@
  * @package
  * @subpackage
  * @copyright Copyright CC BY-NC-ND 4.0 @ 2020 - 2025 Diego Garcia (diego/@/envigo.net)
+ *
+ * RSA 2048 - 245 bytes max -> 60 chars max -> base64 -> varchar(341)
+ * RSA 4096 - 501 bytes max -> 60 chars     -> base64 -> varchar(681)
+ *
+ * Limit to allow both: 60 chars -> varchar(700)
  */
 
 Namespace App\Controllers;
 
-use App\Models\EncryptModel;
 use App\Services\EncryptionService;
 
 
@@ -17,13 +21,12 @@ class EncryptController
 {
     private \AppContext $ctx;
     private EncryptionService $encryptService;
-    private EncryptModel $encryptModel;
+    private int $max_chars = 60;
 
     public function __construct(\AppContext $ctx)
     {
         $this->ctx = $ctx;
 
-        $this->encryptModel = new EncryptModel($this->ctx);
         $this->encryptService = new EncryptionService($this->ctx);
     }
 
@@ -32,14 +35,29 @@ class EncryptController
      * @param string $raw_data
      * @return array<string, string>
      */
+
     public function encrypt(string $raw_data): array
     {
+        if (mb_strlen($raw_data, 'UTF-8') > $this->max_chars) {
+            return [
+                'status' => 'Fail',
+                'error_msg' => 'Input data exceeds the maximum allowed length of ' . $this->max_chars . ' characters.'
+            ];
+        }
+
         try {
+
+            $byte_length = strlen(utf8_decode($raw_data)); // Número de bytes en la cadena UTF-8
+
+            if ($byte_length > 245) {
+                return [
+                    'status' => 'Fail',
+                    'error_msg' => 'Input data exceeds the maximum byte length for RSA encryption (245 bytes).'
+                ];
+            }
             $encryptedData = $this->encryptService->encrypt($raw_data);
 
-            $this->dataModel->saveEncryptedData($encryptedData);
-
-            return ['status' => 'Success'];
+            return ['status' => 'Success', 'response_msg' => $encryptedData];
         } catch (Exception $e) {
             return ['status' => 'Fail' , 'error_msg' => $e->getMessage()];
         }
