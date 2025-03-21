cd0c88ccfdd05f3fae5975428ba0b49554290c42
diegargon <diego@envigo.net>
2025-03-21 21:01:52 +0100

    compose test

--- docker/init.sh
+++ docker/init.sh
@@ -11,10 +11,12 @@ CRON_LINE_1="*/5 * * * * root php /var/www/html/monnet-cli.php"
 CRON_LINE_2="*/15 * * * * root php /var/www/html/monnet-discovery.php"
 CRONTAB_FILE="/etc/crontab"
 
-echo "V.22";
+echo "V.23";
 
 pwd
 ls -al /opt/monnet-core
+cd /opt/monnet-core
+git fetch
 
 # Configurar trabajos cron
 echo "Configurando trabajos cron..."
