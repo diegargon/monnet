53b21737bd01d9999c8354d526c6f6f0d7ddcd64
Tob <Tob>
2025-06-02 17:01:31 +0200

    up

--- config/config.priv.php
+++ config/config.priv.php
@@ -6,7 +6,7 @@
  * @copyright Copyright CC BY-NC-ND 4.0 @ 2020 - 2025 Diego Garcia (diego/@/envigo.net)
  */
 $cfg['monnet_version'] = 0.82;
-$cfg['monnet_revision'] = 2;
+$cfg['monnet_revision'] = 3;
 
 $cfg['monnet_homepage'] = 'https://github.com/diegargon/monnet';
 $cfg['agent_min_version'] = 0.184;
