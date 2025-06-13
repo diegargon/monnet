cc8687ba628b0f6e7d93add2df45dc07244932e0
diegargon <diego@envigo.net>
2025-06-13 21:18:30 +0200

    minor renaming

--- App/Pages/PageDefaults.php
+++ App/Pages/PageDefaults.php
@@ -29,7 +29,7 @@ class PageDefaults
         $page = [];
         $page['theme'] = empty($_user['theme']) ? $ncfg->get('theme') : $_user['theme'];
         $page['lang'] = empty($_user['lang']) ? $default_lang : $_user['lang'];
-        $page['page_charset'] = empty($_user['page_charset']) ? $ncfg->get('default_charset') : $_user['charset'];
+        $page['web_charset'] = empty($_user['web_charset']) ? $ncfg->get('default_charset') : $_user['charset'];
         $page['web_title'] = $ncfg->get('web_title');
 
         return $page;
--- tpl/default/main.tpl.php
+++ tpl/default/main.tpl.php
@@ -3,7 +3,7 @@
  *
  * @author diego/@/envigo.net
  * @copyright Copyright CC BY-NC-ND 4.0 @ 2020 - 2025 Diego Garcia (diego/@/envigo.net)
- * @version 0.5
+ * @version 0.6
  */
 /**
  * In frontend->getTpl()
@@ -14,10 +14,10 @@
 <!DOCTYPE html>
 <html lang="<?= $tdata['lang'] ?>" dir="<?= $tdata['dir'] ?? 'ltr' ?>">
     <head>
-        <meta charset="<?= $tdata['page_charset'] ?>" />
+        <meta charset="<?= $tdata['web_charset'] ?>" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="<?= $tdata['description'] ?? 'Web' ?>">
-        <meta name="keywords" content="<?= $tdata['keywords'] ?? '' ?>">
+        <meta name="description" content="<?= $tdata['web_description'] ?? 'Web' ?>">
+        <meta name="keywords" content="<?= $tdata['web_keywords'] ?? '' ?>">
         <?= $tdata['meta_extra'] ?? '' ?>
         <link rel="shortcut icon" href="favicon.ico" />
         <meta name="referrer" content="never">
